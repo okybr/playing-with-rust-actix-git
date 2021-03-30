39a8a15afce200913c3afc340592e2043668f044
Orhan Kemal Yüksel <orhankemal.yueksel@br.de>
2021-03-30 13:39:00 +0200

    api for creating new repos

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,17 @@ use std::io::Result;
 use std::{fs};
 use std::vec::Vec;
 
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, App, HttpResponse, HttpServer, Responder};
+use actix_web::web::Json;
+use git2::Repository;
+use serde_derive::Deserialize;
+
+
+#[derive(Deserialize)]
+struct RepoNewReq {
+    name: String,
+}
+
 
 #[get("/repo/list")]
 async fn repo_list() -> impl Responder {
@@ -22,9 +32,22 @@ async fn repo_list() -> impl Responder {
     HttpResponse::Ok().body(repositories.join("\n"))
 }
 
+
+#[post("/repo/new")]
+async fn repo_new(req: Json<RepoNewReq>) -> impl Responder {
+    let repo = Repository::init(
+        std::format!("./repositories/{}", req.name));
+
+    match repo {
+        Ok(_) => HttpResponse::Ok().body("okay"),
+        Err(_) => HttpResponse::Ok().body("error")
+    }
+}
+
+
 #[actix_web::main]
 async fn main() -> Result<()> {
-    HttpServer::new(|| { App::new().service(repo_list) })
+    HttpServer::new(|| { App::new().service(repo_list).service(repo_new) })
         .bind("127.0.0.1:8080")?
         .run()
         .await
